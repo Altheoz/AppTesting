b37a13dfadbba3fb175a44202797468f3accab8d
Altheoz <altheozcarl@gmail.com>
2025-09-21 14:42:57 +0800

    Commit - login design

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -20,7 +20,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#007bff',
     borderRadius: 8,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'left',
     elevation: 4, 
     shadowColor: '#000', 
     shadowOffset: { width: 0, height: 2 },
